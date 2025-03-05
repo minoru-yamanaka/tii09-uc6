8140437ec18c04c91be3712cdfb95f4142487ecb
minoru-yamanaka <minoruyamanaka@icloud.com>
2025-03-05 20:42:26 -0300

    add estacionamento

--- projetos/estacionamento/script.js
+++ projetos/estacionamento/script.js
@@ -3,6 +3,7 @@ let infoElementos = document.querySelectorAll(".resumo div");
 let vagas = [null, null, null, null, null, null, null, null, null, null];
 let totalHoras = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 let resultadoTexto = document.getElementById("resultado");
+let totalCarros = 0;
 
 elementosVagas.forEach((vaga, i) => {
     vaga.addEventListener("click", () => gerenciarVaga(i));
@@ -27,6 +28,7 @@ function gerenciarVaga(numeroDaVaga) {
         }
         totalHoras[numeroDaVaga] += hora - vagas[numeroDaVaga];
         vagas[numeroDaVaga] = null;
+        totalCarros++;
         elementosVagas[numeroDaVaga].classList.remove("ocupado");
     }
     atualizarInfo();
@@ -36,4 +38,15 @@ function atualizarInfo() {
     for(let i = 0; i < 10; i++) {
         infoElementos[i].innerText = `Vaga ${i + 1}: ${totalHoras[i]}h`
     }
+}
+
+function totalizar() {
+    let totalHorasDia = 0;
+    
+    for(let i = 0; i < 10; i++) {
+        totalHorasDia += totalHoras[i];        
+    }
+
+    let faturamento = totalHorasDia * 12;
+    resultadoTexto.innerText = `Total de horas: ${totalHorasDia}, Carros Atendidos: ${totalCarros}, Faturamento: R$${faturamento},00`;
 }
